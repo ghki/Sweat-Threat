f0de22d2c7d38ba3a1d1d08773fe24bfa23787c6
Grace Kim <gkim1032@gmail.com>
2016-07-26 10:29:18 -0700

    parallax

--- imports/ui/pages/Home.jsx
+++ imports/ui/pages/Home.jsx
@@ -2,10 +2,25 @@ import React from 'react';
 import classNames from 'classnames';
 
 function Home() {
+	$(document).ready(function(){
+      $('.parallax').parallax();
+    });
   return (
-    <div >
-      <h1>Welcome Home!</h1>
-    </div>
+
+<div>
+<div className="parallax-container">
+	<div className="parallax"><img src="img/pupp.jpg"/></div>
+</div>
+<div className="section white">
+	<div className="row container">
+		<h2 className="header">BLACKMAIL YOURSELF INTO SHAPE</h2>
+		<p className="grey-text text-darken-3 lighten-3">Never miss another workout again…or else.</p>
+	</div>
+</div>
+<div className="parallax-container">
+	<div className="parallax"><img src="img/background1.jpg"/></div>
+</div>
+</div>
   );
 }
 
