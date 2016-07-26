41d71837bedc28580a8b7e217bee9f67fd5aafd3
dan <danho123@gmail.com>
2016-07-26 10:49:13 -0700

    package updates

--- imports/ui/components/Navbar.jsx
+++ imports/ui/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
+import AccountsUIWrapper from '../AccountsUIWrapper.jsx';
+
 
 
 export default class Navbar extends React.Component {
@@ -12,10 +14,10 @@ export default class Navbar extends React.Component {
     return (
       <nav style={navStyle}>
         <div className="nav-wrapper">
-          <a href="/" className="brand-logo">Sweat Threat</a> &nbsp;
+          <a href="/" className="brand-logo">Sweat Threat</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li>  <Link to="/about">About</Link> </li> &nbsp;
-            <li> <Link to="/register">Register</Link> </li> &nbsp;
+            <li>  <Link to="/about">About</Link> </li>
+            <li> <AccountsUIWrapper /> </li>
           </ul>
         </div>
       </nav>
